59db90b95422c6c829c91bbef830a28a0833263a
Hannes Mehnert <hannes@mehnert.org>
2020-10-08 18:07:26 +0200

    initial commit

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+# v3.57
+
+* Initial public release (version numbers meet NSS releases)
---
+++ lib/ca_certs_nss.ml
@@ -0,0 +1,14 @@
+module Make (C : Mirage_clock.PCLOCK) = struct
+  open Rresult.R.Infix
+
+  let trust_anchor ?crls ?hash_whitelist () =
+    List.fold_left
+      (fun acc data ->
+        acc >>= fun acc ->
+        X509.Certificate.decode_der (Cstruct.of_string data) >>| fun cert ->
+        cert :: acc)
+      (Ok []) Trust_anchor.certificates
+    >>| fun tas ->
+    let time () = Some (Ptime.v (C.now_d_ps ())) in
+    X509.Authenticator.chain_of_trust ~time ?crls ?hash_whitelist tas
+end
