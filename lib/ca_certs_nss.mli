ee6aa2d6a2d19b188baff44b8fa14bc7b8b2eb26
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-06-27 05:58:27 +0200

    Remove Cstruct.t from the ca-certs-nss package

--- lib/ca_certs_nss.mli
+++ lib/ca_certs_nss.mli
@@ -1,7 +1,7 @@
 module Make (C : Mirage_clock.PCLOCK) : sig
   val authenticator :
     ?crls:X509.CRL.t list ->
-    ?allowed_hashes:Mirage_crypto.Hash.hash list ->
+    ?allowed_hashes:Digestif.hash' list ->
     unit ->
     (X509.Authenticator.t, [> `Msg of string ]) result
   (** [authenticator ~crls ~hash_whitelist ()] is an authenticator with the
