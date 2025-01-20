6126da3b1659b38d3351f3f85fe6e07b2c802774
Hannes Mehnert <hannes@mehnert.org>
2025-01-20 23:40:23 +0100

    update to using variants

--- lib/ca_certs_nss.ml
+++ lib/ca_certs_nss.ml
@@ -1,16 +1,15 @@
-module Make (C : Mirage_clock.PCLOCK) = struct
-  let authenticator =
-    let tas =
-      List.fold_left
-        (fun acc data ->
-          Result.bind acc (fun acc ->
-              Result.map
-                (fun cert -> cert :: acc)
-                (X509.Certificate.decode_der data)))
-        (Ok []) Trust_anchor.certificates
-    and time () = Some (Ptime.v (C.now_d_ps ())) in
-    fun ?crls ?allowed_hashes () ->
-      Result.map
-        (X509.Authenticator.chain_of_trust ~time ?crls ?allowed_hashes)
-        tas
-end
+let authenticator =
+  let tas =
+    List.fold_left
+      (fun acc data ->
+         Result.bind acc (fun acc ->
+             Result.map
+               (fun cert -> cert :: acc)
+               (X509.Certificate.decode_der data)))
+      (Ok []) Trust_anchor.certificates
+  and time () = Some (Mirage_ptime.now ())
+  in
+  fun ?crls ?allowed_hashes () ->
+    Result.map
+      (X509.Authenticator.chain_of_trust ~time ?crls ?allowed_hashes)
+      tas
