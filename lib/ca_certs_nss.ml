bee72757b760ef4ceda8c6a252b4d7ce5dc94a2c
Hannes Mehnert <hannes@mehnert.org>
2025-07-02 16:06:46 +0200

    provide the trust_anchors separately

--- lib/ca_certs_nss.ml
+++ lib/ca_certs_nss.ml
@@ -1,14 +1,15 @@
+let trust_anchors =
+  List.fold_left
+    (fun acc data ->
+       Result.bind acc (fun acc ->
+           Result.map
+             (fun cert -> cert :: acc)
+             (X509.Certificate.decode_der data)))
+    (Ok []) Trust_anchor.certificates
+
 let authenticator =
-  let tas =
-    List.fold_left
-      (fun acc data ->
-        Result.bind acc (fun acc ->
-            Result.map
-              (fun cert -> cert :: acc)
-              (X509.Certificate.decode_der data)))
-      (Ok []) Trust_anchor.certificates
-  and time () = Some (Mirage_ptime.now ()) in
+  let time () = Some (Mirage_ptime.now ()) in
   fun ?crls ?allowed_hashes () ->
     Result.map
       (X509.Authenticator.chain_of_trust ~time ?crls ?allowed_hashes)
-      tas
+      trust_anchors
--- lib/ca_certs_nss.mli
+++ lib/ca_certs_nss.mli
@@ -1,8 +1,11 @@
+val trust_anchors : (X509.Certificate.t list, [> `Msg of string ]) result
+(** [trust_anchors] are the trust anchors extracted from NSS certdata.txt. *)
+
 val authenticator :
   ?crls:X509.CRL.t list ->
   ?allowed_hashes:Digestif.hash' list ->
   unit ->
   (X509.Authenticator.t, [> `Msg of string ]) result
 (** [authenticator ~crls ~hash_whitelist ()] is an authenticator with the
-    provided revocation lists, hash_whitelist. The trust anchors are based on
-    the extraction from NSS' certdata.txt. *)
+    provided revocation lists, and allowed_hashes. The trust anchors are based
+    on the extraction from NSS' certdata.txt. *)
